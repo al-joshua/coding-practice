36070241ecf9ac938580b55a310388733b2fd20a
al-joshua <aljoshua@MacBook-Air-Alexey.local>
2019-08-29 22:49:25 +0300

    second commit

---
+++ 5kyu Phone Directory.py
@@ -0,0 +1,64 @@
+'''John keeps a backup of his old personal phone book as a text file. On each line of the file he can find the phone number (formated as +X-abc-def-ghij where X stands for one or two digits), the corresponding name between < and > and the address.
+
+Unfortunately everything is mixed, things are not always in the same order; parts of lines are cluttered with non-alpha-numeric characters (except inside phone number and name).
+
+Examples of John's phone book lines:
+
+"/+1-541-754-3010 156 Alphand_St. <J Steeve>\n"
+
+" 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010!\n"
+
+"<Anastasia> +48-421-674-8974 Via Quirinal Roma\n"
+
+Could you help John with a program that, given the lines of his phone book and a phone number returns a string for this number : "Phone => num, Name => name, Address => adress"
+
+Examples:
+
+s = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010!\n"
+
+phone(s, "1-541-754-3010") should return "Phone => 1-541-754-3010, Name => J Steeve, Address => 156 Alphand St."
+It can happen that, for a few phone numbers, there are many people for a phone number -say nb- , then
+
+return : "Error => Too many people: nb"
+
+or it can happen that the number nb is not in the phone book, in that case
+
+return: "Error => Not found: nb"
+
+You can see other examples in the test cases.
+
+JavaScript random tests completed by @matt c
+
+Note
+
+Codewars stdout doesn't print part of a string when between < and >'''
+
+
+import re
+
+
+def phone(strng, num):
+    def regexer(exp, line):
+        reg = re.search(exp, line)
+        p = reg.group(0)
+        return p, line[:reg.start()] + line[reg.end():]
+
+    if strng.count(num) > 1:
+        return "Error => Too many people: {}".format(num)
+
+    if num not in strng:
+        return "Error => Not found: {}".format(num)
+
+    for line in strng.splitlines():
+        if num in line:
+            num, line = regexer('[\d]{1,2}-([\d]{3}-){2}[\d]{4}', line)
+            name, line = regexer('<.*>', line)
+            address = ' '.join(i for i in re.split('[ ?!*$/+;:_,]', line) if i)
+            return "Phone => {}, Name => {}, Address => {}".format(num, name.strip('<>'), address)
+
+
+strng = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010;\n"
+
+
+
+print(phone(strng, '1-541-754-3010'))
---
+++ 5kyu validDate Regex.py
@@ -0,0 +1,39 @@
+'''Your task is to write a regular expression (regex) that will match a string only if it contains at least one valid date, in the format [mm-dd] (that is, a two-digit month, followed by a dash, followed by a two-digit date, surrounded by square brackets).
+
+You should assume the year in question is not a leap year. Therefore, the number of days each month should have are as follows:
+
+1. January - 31 days
+2. February - 28 days (leap years are ignored)
+3. March - 31 days
+4. April - 30 days
+5. May - 31 days
+6. June - 30 days
+7. July - 31 days
+8. August - 31 days
+9. September - 30 days
+10. October - 31 days
+11. November - 30 days
+12. December - 31 days
+All text outside a valid date can be ignored, including other invalid dates.
+
+For example:
+
+"[01-23]" // January 23rd is a valid date
+"[02-31]" // February 31st is an invalid date
+"[02-16]" // valid
+"[ 6-03]" // invalid format
+"ignored [08-11] ignored" // valid
+"[3] [12-04] [09-tenth]" // December 4th is a valid date'''
+
+import re
+
+month = '[01]((?<=0)[1-9]|(?<=1)[0-2])'
+days_30 = '[0-3]((?<=[012])[1-9]|(?<=[123])0)'
+if_30 = '(?<=(0[469]|11)-)'
+days_31 = '[0-3]((?<=[012])[1-9]|(?<=[123])0|(?<=3)1)'
+if_31 = '(?<=(0[13578]|1[02])-)'
+days_28 = '[0-2]((?<=[01])[1-9]|(?<=2)[1-8]|(?<=[12])0)'
+if_28 = '(?<=02-)'
+
+valid_date = re.compile('\[{month}-({if_28}{days_28}|{if_30}{days_30}|{if_31}{days_31})]'.format(**locals()))
+
