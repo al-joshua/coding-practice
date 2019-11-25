2f14559d650301cc3567c06c195b1b28a2031a65
al-joshua <al.joshu@gmail.com>
2019-11-25 15:55:06 +0300

    add 5kyu_Python\ Reflection\:\ Disassembling\ the\ secret.py

---
+++ 5kyu_Python Reflection: Disassembling the secret.py
@@ -0,0 +1,8 @@
+"""There is a string of 32 alphanumeric characters hidden inside a dynamically generated function,
+which will then be passed into your function.
+
+Your task is to recover this string and return it."""
+
+
+def find_the_secret(f):
+    return f.__code__.co_consts[1]
