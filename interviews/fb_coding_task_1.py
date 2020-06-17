55526c9c4dd90c3d022d11bab35135b01dff6fae
al-joshua <al.joshu@gmail.com>
2020-06-17 12:50:44 +0300

    fb task new solution

--- interviews/fb_coding_task_1.py
+++ interviews/fb_coding_task_1.py
@@ -9,6 +9,7 @@ j = 5
 returning array will be  ['a', 'b', 'f', 'e', 'd', 'c', 'g']
 '''
 
+
 def funct_opt1(a, i, j):
     l = []
     for index in range(len(a)):
@@ -20,3 +21,12 @@ def funct_opt1(a, i, j):
 
 def funct_opt2(a, i, j):
     return ','.join(''.join(a[:i]) + ''.join(reversed(a[i:j+1])) + ''.join(a[j+1:])).split(',')
+
+
+def funct_opt3(a, i, j):
+    a = list(a)
+    for k in range(j - i - 1):
+        a[i + k], a[j - k] = a[j - k], a[i + k]
+
+    return a
+
